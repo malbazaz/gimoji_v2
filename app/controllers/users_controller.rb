459316b51c8345c96c6dec788a624c863bcf9026
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 09:55:42 -0400

    Add users index and view page. Add username and fullname columns for admin.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+#before_action :authenticate_user!
+
+    def index
+        @users = User.all
+       # render "users/index"
+    end 
+
+
+    def show 
+        if !!session[:user_id]
+             @user = User.find_by_id(params[:id])
+        else 
+            redirect_to '/users/sign_in'
+        end
+       # render "users/show"
+    end 
+
+end
+
